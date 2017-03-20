d1b21b7df5d77650116934fc61ce4d4716df4793
Joe Dahle <joe.dahle@gmail.com>
2017-03-20 14:50:10 -0700

    refactor: Replace with Header component

--- app/components/main/Main.js
+++ app/components/main/Main.js
@@ -1,15 +1,16 @@
 var React = require('react');
 var ReactRouter = require('react-router');
-var MainContainer = require('../../containers/main/MainContainer');
 var Link = ReactRouter.Link;
-var json = require('../../content/text');
+var MainContainer = require('../../containers/main/MainContainer');
+var Header = require('../../components/Header/Header');
 var style = require('./_index.scss');
+var json = require('../../content/text');
 
 function Main(){
   return (
     <MainContainer>
       <div className="main">
-        <h1 className="main-header">{json.index.head}</h1>
+        <Header />
         <p className="main-about">{json.index.about}</p>
           <Link to='/spreadtheword'>
             <button className="btn" type='button'>Spread the Word</button>
