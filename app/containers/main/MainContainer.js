978dfbf56da69454aa02ee1336061b6f419c1f40
Joe Dahle <joe.dahle@gmail.com>
2017-03-20 22:03:46 -0700

    fix: Correct app structure

--- app/containers/main/MainContainer.js
+++ app/containers/main/MainContainer.js
@@ -1,10 +1,16 @@
+// Core
 var React = require('react');
+
+// components
+var HomeContainer = require('../../containers/home/HomeContainer');
+
+// styles
 var style = require('./_index.scss');
 
 function MainContainer(props) {
     return (
       <div className='main-container'>
-        {props.children}
+        <HomeContainer />
       </div>
     );
   }
---
+++ app/containers/main/README.md
@@ -0,0 +1,14 @@
+## Main container component
+___
+
+A container for containers
+
+### Example
+```js
+<MainContainer />
+```
+
+### Props
+|Prop       | Type      | Default     | Possible Values
+|-----------|-----------|-------------|-----------------------
+|**none     |           |             | 
