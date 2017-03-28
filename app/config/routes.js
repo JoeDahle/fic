31d2720d66164a7bcb57753e92aa540f5938e0c8
Joe Dahle <joe.dahle@gmail.com>
2017-03-27 17:47:34 -0700

    refactor: Update import/export

--- app/config/routes.js
+++ app/config/routes.js
@@ -7,21 +7,10 @@ import { hashHistory } from 'react-router';
 import { IndexRoute } from 'react-router';
 import { IndexRedirect } from 'react-router';
 
-// var React = require('react');
-// var ReactRouter = require('react-router');
-// var Router = ReactRouter.Router;
-// var Route = ReactRouter.Route;
-// var hashHistory = ReactRouter.hashHistory;
-// var IndexRoute = ReactRouter.IndexRoute;
-// var IndexRedirect = ReactRouter.IndexRedirect;
-
 // components
 import MainContainer from '../containers/main/MainContainer';
 import HomeContainer from '../containers/home/HomeContainer';
 import ShareContainer from '../containers/share/ShareContainer';
-// var MainContainer = require('../containers/main/MainContainer');
-// var HomeContainer = require('../containers/home/HomeContainer');
-// var ShareContainer = require('../containers/share/ShareContainer');
 
 var routes = (
   <Router history={hashHistory}>
